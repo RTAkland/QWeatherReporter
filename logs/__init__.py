2c5c97fc6306588b0bc81dae1922e40ff6720273
RTAkland <markushammered@gmail.com>
2021-11-18 22:21:01 +0800

    initial: initial __init__.py

---
+++ logs/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -- coding:utf-8 --
+# @Author: markushammered@gmail.com
+# @Development Tool: PyCharm
+# @Create Time: 2021/11/18
+# @File Name: __init__.py
+
+print('This file is used for stating this folder is not an empty directory')

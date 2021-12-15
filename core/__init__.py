e0dcf8af56e3f493089ae1c6aa02e158dd7342b7
RTAkland <markushammered@gmail.com>
2021-12-15 21:26:36 +0800

    feat: check settings

--- core/__init__.py
+++ core/__init__.py
@@ -4,3 +4,28 @@
 # @Development Tool: PyCharm
 # @Create Time: 2021/12/15
 # @File Name: __init__.py
+
+"""
+Check the configuration file
+"""
+
+import sys
+from ruamel.yaml import YAML
+from core.logger import Logger
+
+
+with open('./config.yml', 'r') as f:
+    settings = YAML().load(f.read())
+
+for mail in settings['mail-settings'].values():
+    if not mail:
+        Logger.critical('mail-settings 有未填写项目')
+        sys.exit(1)
+for request in settings['request-settings'].values():
+    if not request:
+        Logger.critical('request-settings 有未填写项目')
+        sys.exit(1)
+for other in settings['client-settings'].values():
+    if not other:
+        Logger.critical('client-settings 有未填写项目')
+        sys.exit(1)
--- tests/s.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -- coding:utf-8 --
-# @Author: markushammered@gmail.com
-# @Development Tool: PyCharm
-# @Create Time: 2021/12/15
-# @File Name: s.py

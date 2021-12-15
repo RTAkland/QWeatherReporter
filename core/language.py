1db1d0f545791eee8693a1fdc3d317c74c0853da
RTAkland <markushammered@gmail.com>
2021-12-15 21:47:44 +0800

    feat: language.py

--- core/language.py
+++ core/language.py
@@ -4,3 +4,20 @@
 # @Development Tool: PyCharm
 # @Create Time: 2021/12/15
 # @File Name: language.py
+
+from ruamel.yaml import YAML
+import json
+
+
+def Language():
+    with open('./config.yml', 'r', encoding='utf-8') as lang:
+        config = YAML().load(lang.read())
+        language_sel = config['client-settings']['language']
+        if language_sel not in ['zh_cn', 'en_us']:
+            language_sel = 'zh_cn'
+
+    # 打开语言json文件
+    with open(f'./res/lang/{language_sel}.json', 'r', encoding='utf-8') as lang_f:
+        language = json.loads(lang_f.read())
+
+    return language

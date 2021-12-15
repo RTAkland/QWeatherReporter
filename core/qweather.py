9767bc9ef1f0c4e1c6a91f1a239e308ce13dc06b
RTAkland <markushammered@gmail.com>
2021-12-15 22:08:57 +0800

    feat: qweather.py

--- core/qweather.py
+++ core/qweather.py
@@ -4,3 +4,11 @@
 # @Development Tool: PyCharm
 # @Create Time: 2021/12/15
 # @File Name: qweather.py
+
+from core.logger import Logger
+from core.language import Language
+from core import settings
+from core import read_excel
+from core import read_config
+from core import information
+

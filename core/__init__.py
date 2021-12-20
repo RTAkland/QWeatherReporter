28e1319ee34216de8c1bfec7b8b741621a40d873
RTAkland <markushammered@gmail.com>
2021-12-20 21:22:42 +0800

    feat: fixed many bugs

--- core/__init__.py
+++ core/__init__.py
@@ -5,31 +5,8 @@
 # @Create Time: 2021/12/15
 # @File Name: __init__.py
 
-"""
-Check the configuration file
-"""
 
 import os
-import sys
-from core.logger import Logger
-from core.language import Language
-from core.read_config import read_config
-
-
-settings = read_config()
 
 if not os.path.exists('./logs'):
     os.mkdir('./logs')
-
-for mail in settings[0].values():
-    if not mail:
-        Logger.critical('mail-settings 有未填写项目')
-        sys.exit(1)
-for request in settings[1].values():
-    if not request:
-        Logger.critical('request-settings 有未填写项目')
-        sys.exit(1)
-for other in settings[1].values():
-    if not other:
-        Logger.critical('client-settings 有未填写项目')
-        sys.exit(1)

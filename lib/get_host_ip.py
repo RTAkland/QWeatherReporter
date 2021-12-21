62d08096b36289eb6a33b0bca7cf470f0490623a
RTAkland <markushammered@gmail.com>
2021-12-21 20:54:38 +0800

    feat: added get_host_ip.py

--- lib/get_host_ip.py
+++ lib/get_host_ip.py
@@ -3,3 +3,18 @@
 # @Development Tool: PyCharm
 # @Create Time: 2021/12/21
 # @File Name: get_host_ip.py
+
+import socket
+
+
+def get_host_ip():
+    """
+    查询本机ip地址
+    :return: ip
+    """
+    ip_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ip_s.connect(('8.8.8.8', 80))
+    ip = ip_s.getsockname()[0]
+    ip_s.close()
+
+    return ip

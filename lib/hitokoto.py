ea4d04a71c905e8e5bda91e20bf7258dd19b2a6d
RTAkland <markushammered@gmail.com>
2021-12-24 19:51:49 +0800

    feat: add hitokoto

--- lib/hitokoto.py
+++ lib/hitokoto.py
@@ -3,3 +3,21 @@
 # @Development Tool: PyCharm
 # @Create Time: 2021/12/24
 # @File Name: hitokoto.py
+
+import requests
+from core.language import Language
+from core.logger import Logger
+
+
+def hitokoto():
+    language = Language()
+    url = 'https://v1.hitokoto.cn/'
+    Logger.info(f'{language["hitokoto"]}')
+    res = requests.get(url).json()
+    speaker = res['from_who']
+    text = res['hitokoto']
+    if not speaker:
+        speaker = language['noname']
+    full_text = f'{text}  --{speaker}'
+
+    return full_text

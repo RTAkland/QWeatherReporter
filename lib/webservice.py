b41be502f2aa104c21a763e2277c522340345df7
RTAkland <markushammered@gmail.com>
2022-01-01 15:34:45 +0800

    feat: fix bug

--- lib/webservice.py
+++ lib/webservice.py
@@ -13,10 +13,13 @@ from core.read_config import read_config
 from core.information import WeatherInfo
 from core.language import Language
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind(('0.0.0.0', 7898))
-server.listen(5)
+try:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(('0.0.0.0', 7898))
+    server.listen(5)
+except OSError as e:
+    Logger.critical(e)
 
 language = Language()
 
